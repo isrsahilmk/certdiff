afedfdc8e5088103ab7a02b7c25c7aada15b0709
Sahil Tembhare <isr.sahil.mk@gmail.com>
2020-03-17 01:58:53 +0530

    Tried my best to handle the error, but was unsuccessful so currently using .expect() which gives an ugly error

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ extern crate clap;
 
 use clap::{Arg, App};
 
-use std::env;
+use std::env::var;
+use std::fs;
+
 
 fn main() {
 
@@ -10,7 +12,7 @@ fn main() {
     let arg_matches = App::new("facediff")
         .version("0.0.1")
         .author("Sahil Tembhare @isrsahilmk")
-        .about("Facebook cert diffing tool for continuous subdomain reconnaissance, can also be used for single use. (This tool will save all the data into ~/.facediff directory")
+        .about("Facebook cert diffing tool for continuous subdomain reconnaissance, single use as well. (This tool will save all the data into ~/.facediff directory")
         .arg(Arg::with_name("target")
             .short("t")
             .long("target")
@@ -24,17 +26,38 @@ fn main() {
             .help("Enter your facebook API token which will be used to access the facebook cert transparency tool")
             .takes_value(true)).get_matches();
 
-    let target = arg_matches.value_of("target");
+    // checking if ~/.facediff dir exists, if not, create one            
+    match fs::create_dir(format!("{}/.facediff", var("HOME").unwrap().to_string())) {
+        Ok(_) => (),
+        Err(_) => ()
+    }
 
-    match target {
+    // save the api token
+    match arg_matches.value_of("facebook_api_token") {
+        Some(api_key) => {
+            match fs::write(format!("{}/.facediff/.apikey", var("HOME").unwrap().to_string()), api_key) {
+                Ok(()) => println!("Api key saved succesfully."),
+                Err(_) => ()
+            }
+        },
+        None => ()
+    }
+
+    // call the client
+    match arg_matches.value_of("target") {
         Some(target) => {
-            client(&target.to_string());
+            match client(&target.to_string()) {
+                Ok(()) => (),
+                Err(_) => ()
+            }
         },
         None => println!("Target arg not passed, see help using --help")
     }
 }
 
 fn client(target: &String) -> Result<(), Box<dyn std::error::Error>> {
+
+    let apikey = fs::read_to_string(format!("{}/.facediff/.apikey", var("HOME")?.to_string())).expect("Unable to read the apikey");
 
     let response = reqwest::blocking::get(target)?.text_with_charset("utf-8")?;
     println!("Response: {}", &response);
