eab90ee462746eaba114607a8a681ff4a6efa085
Sahil Tembhare <isr.sahil.mk@gmail.com>
2020-03-19 03:16:14 +0530

    Made so many updates, 1. parses json 2. Creates folders for target

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,43 @@
 extern crate clap;
+extern crate json;
 
 use clap::{Arg, App};
 
-use std::env::var;
+use std::env;
 use std::fs;
-
+ 
 
 fn main() {
 
     // clap thang
-    let arg_matches = App::new("facediff")
+    let arg_matches = App::new("certdiff")
         .version("0.0.1")
         .author("Sahil Tembhare @isrsahilmk")
-        .about("Facebook cert diffing tool for continuous subdomain reconnaissance, single use as well. (This tool will save all the data into ~/.facediff directory")
+        .about("sslmate cert diffing tool for continuous subdomain reconnaissance, single use as well. (This tool will save all the data into ~/.certdiff directory")
         .arg(Arg::with_name("target")
             .short("t")
             .long("target")
             .value_name("target domain")
             .help("Enter the target site, you can keep track of subdomains of the target site")
-            .takes_value(true))
-        .arg(Arg::with_name("facebook_api_token")
-            .short("a")
-            .long("api")
-            .value_name("facebook api token")
-            .help("Enter your facebook API token which will be used to access the facebook cert transparency tool")
             .takes_value(true)).get_matches();
 
-    // checking if ~/.facediff dir exists, if not, create one            
-    match fs::create_dir(format!("{}/.facediff", var("HOME").unwrap().to_string())) {
-        Ok(_) => (),
-        Err(_) => ()
+      
+    // checking if ~/.facediff dir exists, if not, create one
+    match env::home_dir() {
+   	 Some(home_path) => {
+	    match fs::create_dir(home_path.join(".certdiff")) {
+	        Ok(_) => (),
+		    Err(_) => ()
+	    }
+	 },
+	 None => println!("No home dir")
     }
 
-    // save the api token
-    match arg_matches.value_of("facebook_api_token") {
-        Some(api_key) => {
-            match fs::write(format!("{}/.facediff/.apikey", var("HOME").unwrap().to_string()), api_key) {
-                Ok(()) => println!("Api key saved succesfully."),
-                Err(_) => ()
-            }
-        },
-        None => ()
-    }
 
-    // call the client
+    // call the http client
     match arg_matches.value_of("target") {
         Some(target) => {
-            match client(&target.to_string()) {
+            match http_client(&target.to_string()) {
                 Ok(()) => (),
                 Err(_) => ()
             }
@@ -55,11 +46,39 @@ fn main() {
     }
 }
 
-fn client(target: &String) -> Result<(), Box<dyn std::error::Error>> {
 
-    let apikey = fs::read_to_string(format!("{}/.facediff/.apikey", var("HOME")?.to_string())).expect("Unable to read the apikey");
 
-    let response = reqwest::blocking::get(target)?.text_with_charset("utf-8")?;
-    println!("Response: {}", &response);
+fn http_client(target: &str) -> Result<(), Box<dyn std::error::Error>> {
+
+    let url = format!("https://api.certspotter.com/v1/issuances?domain={}&include_subdomains=true&expand=dns_names", target);
+    let response = reqwest::blocking::get(&url)?.text()?;
+
+    // json parsing
+    let parsed_json = json::parse(&response).unwrap();
+   
+    create_target_dir(&target, parsed_json);
+
     return Ok(());
+}
+
+fn create_target_dir(target: &str, parsed_json: json::JsonValue) {
+
+    match env::home_dir() {
+        Some(home_path) => {
+            match fs::create_dir(home_path.join(".certdiff").join(target)) {
+                Ok(_) => {
+                    println!("Directory created for {}, at {} \n", target, home_path.join(".certdiff").join("target").display());
+                    for data in parsed_json.members() {
+                        for domain in data["dns_names"].members() {
+                            println!("{}", domain);
+                        }
+                    }
+                },
+                Err(_) => {
+                    // check if first file exists -> target(1), and create one more for diffing -> target(2)
+                }
+            }
+        },
+        None => println!("No home dir")
+    }
 }
